c394c6ace7f3513290b40533f70e33ee7b2f098f
BalVil <balutavili@gmail.com>
2022-07-06 02:49:03 +0300

    Task 1 (Feedback widget) is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { Container } from './App.styled';
+import { Statistics } from 'components/Statistics';
+import { FeedbackOptions } from 'components/FeedbackOptions';
+import { Section } from 'components/Section';
+import { Notification } from 'components/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleRate = rating => {
+    this.setState(prevState => ({ [rating]: prevState[rating] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const ratingValues = Object.values(this.state);
+    return ratingValues.reduce((acc, value) => acc + value, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const ratingNames = Object.keys(this.state);
+
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={ratingNames}
+            onLeaveFeedback={this.handleRate}
+          />
+        </Section>
+
+        <Section title="Statistic">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
